ae9ab29f9d57315bb706076b0ad0ed747ad2265b
AashisBhujel <ashisbhujel331@gmail.com>
2024-01-22 15:09:36 +0545

    8th commit

--- Day-5/easy.js
+++ Day-5/easy.js
@@ -1,26 +1,77 @@
-const url =
-  'https://opentdb.com/api.php?amount=10&category=11&type=multiple&encode=url3986';
+let questions;
 
-async function getData() {
-  // const request = fetch(url);
-  // const response = (await request).json;
-  // console.log(request);
+function fetchData() {
+  fetch('https://opentdb.com/api.php?amount=10&difficulty=easy')
+    .then((response) => response.json())
+    .then((data) => {
+      questions = data.results.map((question) => {
+        const formattedQuestion = {
+          question: question.question,
+          answers: [...question.incorrect_answers, question.correct_answer],
+          correctIndex: question.incorrect_answers.length,
+        };
+        return formattedQuestion;
+      });
+      startQuiz();
+    })
+    .catch((error) => console.log('Error fetching data', error));
+}
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log('Error1:', error);
+const questionElement = document.getElementById('question');
+const answerButton = document.getElementById('answer-button');
+const nextButton = document.getElementById('next-btn');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = 'Next';
+  showQuestion();
+}
+
+function showQuestion() {
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + '.' + currentQuestion.question;
+
+  answerButton.innerHTML = '';
+
+  currentQuestion.answers.forEach((answer, index) => {
+    const button = document.createElement('button');
+    button.classList.add('btn');
+    button.innerHTML = answer;
+    button.addEventListener('click', () => checkAnswer(index));
+    answerButton.appendChild(button);
+  });
+}
+
+function checkAnswer(selectedIndex) {
+  const currentQuestion = questions[currentQuestionIndex];
+
+  if (selectedIndex === currentQuestion.correctIndex) {
+    score++;
+    alert('Correct!');
+    moveToNextQuestion();
+  } else {
+    alert(
+      'Incorrect! The correct answer is: ' +
+        currentQuestion.answers[currentQuestion.correctIndex]
+    );
   }
 }
 
-function displayQuestion(questions) {
-  const questionContainer = document.getElementById('questionContainer');
-  let currentQuestionsIndex = 0;
-  console.log(currentQuestionsIndex[0]);
+function moveToNextQuestion() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    alert('Quiz completed. Your score: ' + score);
+    if (confirm('Do you want to restart the quiz?')) {
+      startQuiz();
+    }
+  }
 }
 
-function showCurrentQuestion() {}
-
-getData();
+fetchData();
